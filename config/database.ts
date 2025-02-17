c40d35e5fcc1d4974127d01686d7507529d4435a
Cláudio Melgaço <claudio19a@hotmail.com>
2025-02-17 06:11:55 -0300

    Testanto conexão com o banco

--- config/database.ts
+++ config/database.ts
@@ -3,17 +3,14 @@ import { User } from '../src/user/user.model';
 import { Supplier } from '../src/supplier/supplier.model';
 import { Contract } from '../src/contract/contract.model';
 import { Review } from '../src/review/review.model';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const databaseConfig: SequelizeModuleOptions = {
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '1234',
-  database: process.env.DB_NAME || 'graphql_db',
+  host: 'dpg-cupfioa3esus738de2n0-a.ohio-postgres.render.com',
+  port: 5432,
+  username: 'clarke_db_0z08_user',
+  password: 'qZHi1XQEbz55DQVrN59mtLrUsVk5UFuC',
+  database: 'clarke_db_0z08',
   models: [User, Supplier, Contract, Review],
   autoLoadModels: true,
   synchronize: false,
